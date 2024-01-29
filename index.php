43c5f46d907ee9ec2148db3773de4eb1c03b004b
albertomandirola <albymandi00@gmail.com>
2024-01-29 16:38:39 +0100

    exercise done create todolist without bonus

--- index.php
+++ index.php
@@ -3,32 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+    
+    <title>todolist</title>
 </head>
 <body>
-    <header class='text-center'>
-        
-        <h1>TODOLIST</h1>
-                   
-    </header>
-    <main>
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-8">
-                    <ul class="list-unstyled">
-                        <li>cose da fare</li>
-                        <li>cose da fare</li>
-                        <li>cose da fare</li>
-                        <li>cose da fare</li>
-                    </ul>
+    <div id='app'>
+        <header class='text-center'>
+            <h1>TODOLIST</h1>      
+        </header>
+        <main>
+            <div class="container ">
+                <div class="row justify-content-center">
+                    <div class="col-8">
+                        <ul class="list-unstyled">
+                            <li class='py-2' v-for="todo, key in todoList" :key="key">
+                                {{todo.name}}
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </main>
-    <script src="./js/script.js"></script>
+            </div>  
+        </main>
+    </div>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.7/axios.min.js" integrity="sha512-NQfB/bDaB8kaSXF8E77JjhHG5PM6XVRxvHzkZiwl3ddWCEPBa23T76MuWSwAJdMGJnmQqM0VeY9kFszsrBEFrQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="js/script.js"></script>
 </body>
 </html>
--- server.php
+++ server.php
@@ -1,1 +1,11 @@
-<?php?>
+<?php
+// Create variable with value todolist in file json
+$todoList = file_get_contents('todo-list.json');
+
+// decode the list take in json in associative array 
+$list = json_decode($todoList, true);
+
+header('Content-Type: application/json');
+
+//send back the information in json form
+echo json_encode($list);
